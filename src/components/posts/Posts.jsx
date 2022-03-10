e00d5fa0c6e1b91eca726bb51387df643ffc1214
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-03-10 20:50:41 -0300

    added XMLHttpRequest exchange token

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,60 +1,60 @@
-import Post from './Post.jsx';
+import Post from "./Post.jsx";
 
-import '../../css/posts.css';
+import "../../css/posts.css";
 
 const placeholderData = [
-	{
-		avatar: 'img/meowed.svg',
-		username: 'meowed',
-		postImg: 'img/gato-telefone.svg',
-		likedByImg: 'img/respondeai.svg',
-		likedByUser: 'respondeai',
-	},
-	{
-		avatar: 'img/meowed.svg',
-		username: 'meowed',
-		img: 'img/gato-telefone.svg',
-		likedByImg: 'img/respondeai.svg',
-		likedByUser: 'respondeai',
-	},
-	{
-		avatar: 'img/meowed.svg',
-		username: 'meowed',
-		img: 'img/gato-telefone.svg',
-		likedByImg: 'img/respondeai.svg',
-		likedByUser: 'respondeai',
-	},
-	{
-		avatar: 'img/meowed.svg',
-		username: 'meowed',
-		img: 'img/gato-telefone.svg',
-		likedByImg: 'img/respondeai.svg',
-		likedByUser: 'respondeai',
-	},
-	{
-		avatar: 'img/meowed.svg',
-		username: 'meowed',
-		img: 'img/gato-telefone.svg',
-		likedByImg: 'img/respondeai.svg',
-		likedByUser: 'respondeai',
-	},
+  {
+    avatar: "img/meowed.svg",
+    username: "meowed",
+    postImg: "img/gato-telefone.svg",
+    likedByImg: "img/respondeai.svg",
+    likedByUser: "respondeai",
+  },
+  {
+    avatar: "img/meowed.svg",
+    username: "meowed",
+    img: "img/gato-telefone.svg",
+    likedByImg: "img/respondeai.svg",
+    likedByUser: "respondeai",
+  },
+  {
+    avatar: "img/meowed.svg",
+    username: "meowed",
+    img: "img/gato-telefone.svg",
+    likedByImg: "img/respondeai.svg",
+    likedByUser: "respondeai",
+  },
+  {
+    avatar: "img/meowed.svg",
+    username: "meowed",
+    img: "img/gato-telefone.svg",
+    likedByImg: "img/respondeai.svg",
+    likedByUser: "respondeai",
+  },
+  {
+    avatar: "img/meowed.svg",
+    username: "meowed",
+    img: "img/gato-telefone.svg",
+    likedByImg: "img/respondeai.svg",
+    likedByUser: "respondeai",
+  },
 ];
 
 export default function Posts(props) {
-	const content = props.content ? props.content : placeholderData;
+  const content = props.content ? props.content : placeholderData;
 
-	return (
-		<section className="posts">
-			{content.slice(0, 12).map((post, index) => (
-				<Post
-					userAvatar={post.avatar}
-					username={post.username}
-					postImg={post.img}
-					likedByImg={post.likedByImg}
-					likedByUser={post.likedByUser}
-					key={post.id}
-				/>
-			))}
-		</section>
-	);
+  return (
+    <section className="posts">
+      {content.slice(0, 12).map((post, index) => (
+        <Post
+          userAvatar={post.avatar}
+          username={post.username}
+          postImg={post.img}
+          likedByImg={post.likedByImg}
+          likedByUser={post.likedByUser}
+          key={index}
+        />
+      ))}
+    </section>
+  );
 }
--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -1,39 +1,41 @@
-import Story from './Story.jsx';
+import Story from "./Story.jsx";
 
-import '../../css/stories.css';
+import "../../css/stories.css";
 
 const placeholderData = [
-	{ img: 'img/9gag.svg', user: '9gag' },
-	{ img: 'img/meowed.svg', user: 'meowed' },
-	{ img: 'img/barked.svg', user: 'barked' },
-	{
-		img: 'img/nathanwpylestrangeplanet.svg',
-		user: 'nathanwpylestrangeplanet',
-	},
-	{ img: 'img/wawawicomics.svg', user: 'wawawicomics' },
-	{ img: 'img/respondeai.svg', user: 'respondeai' },
-	{ img: 'img/filomoderna.svg', user: 'filomoderna' },
-	{ img: 'img/memeriagourmet.svg', user: 'memeriagourmet' },
-	{ img: 'img/9gag.svg', user: '9gag' },
+  { img: "img/9gag.svg", user: "9gag" },
+  { img: "img/meowed.svg", user: "meowed" },
+  { img: "img/barked.svg", user: "barked" },
+  {
+    img: "img/nathanwpylestrangeplanet.svg",
+    user: "nathanwpylestrangeplanet",
+  },
+  { img: "img/wawawicomics.svg", user: "wawawicomics" },
+  { img: "img/respondeai.svg", user: "respondeai" },
+  { img: "img/filomoderna.svg", user: "filomoderna" },
+  { img: "img/memeriagourmet.svg", user: "memeriagourmet" },
+  { img: "img/9gag.svg", user: "9gag" },
 ];
 
 /* Randomize array using Durstenfeld shuffle algorithm */
 function shuffleArray(array) {
-	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
-	}
-	return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 export default function Stories(props) {
-	const content = props.content ? shuffleArray([...props.content]) : placeholderData;
+  const content = props.content
+    ? shuffleArray([...props.content])
+    : placeholderData;
 
-	return (
-		<section className="stories">
-			{content.map((story) => (
-				<Story img={story.avatar} user={story.username} key={story.id} />
-			))}
-		</section>
-	);
+  return (
+    <section className="stories">
+      {content.map((story, index) => (
+        <Story img={story.avatar} user={story.username} key={index} />
+      ))}
+    </section>
+  );
 }
